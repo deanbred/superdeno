5f3bd3e6dbb838d3f40dc1798e91af8fc3798299
deanbred <deanbred@gmail.com>
2020-05-23 00:49:22 -0400

    commit on 'master'

---
+++ branches-tutorial/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BranchesAndLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 5;
+            int b = 5;
+            if (a + b > 10)
+                Console.WriteLine("The answer is greater than 10.");
+        }
+    }
+}
