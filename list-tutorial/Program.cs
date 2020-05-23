8d541d23a65a5747e490213550bd80861627a73d
deanbred <deanbred@gmail.com>
2020-05-23 03:20:44 -0400

    commit

--- list-tutorial/Program.cs
+++ list-tutorial/Program.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace list_tutorial
+namespace CollectionsTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var names = new List<string> { "Deno", "Ana", "Filipe"};
+            var names = new List<string> { "Dean0", "Ana", "Felipe" };
             foreach (var name in names)
             {
-                Console.WriteLine("Hello World!");
+                Console.WriteLine($"Hello {name.ToUpper()}!");
             }
+
+            Console.WriteLine();
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Add("Ray");
+            names.Remove("Ana");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            Console.WriteLine($"My name is {names[0]}");
+            Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+
+            Console.WriteLine($"The list has {names.Count} people");
         }
     }
 }
